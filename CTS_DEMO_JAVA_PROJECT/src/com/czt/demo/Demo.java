e49ea3b07f0253ec9dead4a1e47d27149643067c
zadmin <zadmin@vm-lab154-30-430.asia-south1-a.c.niit-stackroute-2.internal>
2025-11-25 16:07:29 +0530

    Committing first STS Java Project

---
+++ CTS_DEMO_JAVA_PROJECT/src/com/czt/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.czt.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the STS Project");
+	}
+
+}
---
+++ CTS_DEMO_JAVA_PROJECT/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CTS_DEMO_JAVA_PROJECT {
+}
